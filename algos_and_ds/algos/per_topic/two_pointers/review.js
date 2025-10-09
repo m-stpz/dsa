291acd428189757b5e08e78c10d2288c903d95b8
matt-fonto <mateus.fonto@gmail.com>
2025-10-09 15:29:53 +0200

    review: two sum sorted

--- algos_and_ds/algos/per_topic/two_pointers/review.js
+++ algos_and_ds/algos/per_topic/two_pointers/review.js
@@ -49,7 +49,66 @@ function moveZeroes(nums) {
 moveZeroes([0, 1, 0, 3, 12]);
 
 /* 
-2. Remove duplicates
+2. Two Sum Sorted
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+Design algo
+2 7 11 15
+i
+2 7 11 15
+       j => naive, nested, O(n^2)
+
+2 7 11 15
+p1
+        p2
+
+2 + 15 = 17
+    if sum === target
+        return
+         3        9
+    if sum < target
+        increase p1
+    if sum > target
+        decrease p2
+*/
+
+function twoSumSorted(numbers, target) {
+  let start = 0;
+  let end = numbers.length - 1;
+
+  while (start < end) {
+    let sum = numbers[start] + numbers[end];
+
+    if (sum === target) {
+      return [start + 1, end + 1];
+    }
+
+    if (sum < target) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+
+  return -1;
+}
+
+twoSumSorted([2, 7, 11, 15], 9);
+
+/* 
+3. Remove duplicates
 
 Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
 
@@ -61,7 +120,6 @@ Change the array nums such that the first k elements of nums contain the unique 
 The remaining elements of nums are not important as well as the size of nums.
 Return k.
 
-
 Input: nums = [1,1,2]
 Output: 2, nums = [1,2,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
@@ -71,22 +129,4 @@ Input: nums = [0,0,1,1,1,2,2,3,3,4]
 Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
-*/
-
-/* 
-3. Two Sum Sorted
-Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
-
-Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
-
-The tests are generated such that there is exactly one solution. You may not use the same element twice.
-
-Your solution must use only constant extra space.
-
-
-Example 1:
-
-Input: numbers = [2,7,11,15], target = 9
-Output: [1,2]
-Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
 */
