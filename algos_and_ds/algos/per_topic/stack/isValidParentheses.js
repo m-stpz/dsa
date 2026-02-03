cc0499f2c5aaa5d4c5c963960b9350e7d7ee5d2d
matt-fonto <mateus.fonto@gmail.com>
2026-02-03 20:02:19 +0100

    valid parenthesis

--- algos_and_ds/algos/per_topic/stack/isValidParentheses.js
+++ algos_and_ds/algos/per_topic/stack/isValidParentheses.js
@@ -1,1 +1,29 @@
-var isValid = function (s) {};
+var isValid = function (s) {
+  const validOpenings = new Set("(", "[", "{");
+  const stack = [];
+
+  for (const char of s) {
+    if (validOpenings.has(char)) {
+      switch (char) {
+        case "(":
+          stack.push(")");
+          break;
+        case "[":
+          stack.push("]");
+          break;
+        default:
+          stack.push("}");
+          break;
+      }
+    } else {
+      // how to deal with empty stack?
+      if (s[char] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0; // after having ran through all possible scenarios
+};
