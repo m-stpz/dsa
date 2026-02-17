d5de3f8a89c2227f2d07dbcaa13ee32b9ff75f92
matt-fonto <mateus.fonto@gmail.com>
2026-02-17 19:38:14 +0100

    evaluate reverse polish notation

---
+++ algos_and_ds/algos/per_topic/stack/evaluateReversePolishNotation.js
@@ -0,0 +1,76 @@
+/* 
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+ 
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+Example 2:
+
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+Example 3:
+
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+
+*/
+
+var evalRPN = function (tokens) {
+  const operators = new Set(["+", "-", "*", "/"]);
+  const stack = [];
+
+  for (let char of tokens) {
+    if (operators.has(char) && stack.length >= 2) {
+      const firstElement = Number(stack.pop());
+      const secondElement = Number(stack.pop());
+
+      const result = calculateResult(firstElement, secondElement, char);
+      stack.push(result);
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return Number(stack[0]);
+};
+
+function calculateResult(firstElement, secondElement, char) {
+  switch (char) {
+    case "+":
+      return secondElement + firstElement;
+    case "-":
+      return secondElement - firstElement;
+    case "*":
+      return secondElement * firstElement;
+    case "/":
+      const result = secondElement / firstElement;
+
+      if (result >= 0) {
+        return Math.floor(result);
+      } else {
+        return Math.ceil(result);
+      }
+  }
+}
+
+evalRPN(["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]);
